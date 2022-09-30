e330290221174ed584c3b4a01fbd8197035d4ec8
Nacho Rondan <nacho.rondan@gmail.com>
2022-09-29 21:15:38 -0300

    Agrego layout general, y arranco feed de comentarios

--- src/pages/Course.js
+++ src/pages/Course.js
@@ -1,11 +1,12 @@
-import { Stack, CardContent, Paper, Box, Container, Divider, Typography, Card, CardActionArea, CardMedia, Avatar, CardHeader } from "@mui/material"
+import { Stack, TextField, Paper, Box, Container, Typography, Card, CardActionArea, CardMedia, Avatar, CardHeader, Divider } from "@mui/material"
 import { useParams } from "react-router-dom"
 import getCourses from "../assets/MockUpVariables/MockUpCourses"
 import getUsers from "../assets/MockUpVariables/MockUpUsers"
+import Calification from '../components/Calification'
 
 function findCourse(courseId) {
     return getCourses().find((element) => {
-      return element.courseId == courseId;
+      return element.courseId === parseInt(courseId)
     })
 }
 
@@ -23,13 +24,14 @@ export default function Course({user}) {
     return (
         <Box flex={12} >
             <Container>
-                <Stack padding={2} spacing={2}>
+                <Stack padding={2} spacing={2} divider={<Divider/>}>
                     <Paper elevation={3}>
-                        <Box sx={{ display:'flex', flexDirection: 'column', alignContent: 'space-around'}}>
+                        <Box marginBottom={2} sx={{ display:'flex', flexDirection: 'column'}}>
                             <Box p={2} marginBottom={1}>
                                 <Typography variant="h3">{course.courseTitle}</Typography>
                             </Box>
-                            <Box px={2} sx={{ display:'flex', flexDirection: 'row', justifyContent: 'space-around'}}>
+                            <Container ><Divider/></Container>
+                            <Box marginTop={2} px={2} sx={{ display:'flex', flexDirection: 'row'}}>
                                 <Card sx={{ maxWidth: 345 }}>
                                     <CardHeader
                                         avatar={
@@ -44,14 +46,69 @@ export default function Course({user}) {
                                         />
                                     </CardActionArea>
                                 </Card>
-                                <Container>
-                                    Datos Curso
+                                <Container >
+                                    <Stack divider={<Divider/>}>
+                                        <Box p={2} sx={{display:'flex', justifyContent:'space-around', }}>
+                                            <Box sx={{display:'flex', justifyContent:'center', }}>
+                                                <Typography variant="h5" display='flex' flex={8} >Calificacion</Typography>
+                                            </Box>
+                                            <Box sx={{display:'flex', justifyContent:'center', }}>
+                                                <Calification calification={course.calification}/>
+                                            </Box>
+                                        </Box>
+                                        <Box p={2} sx={{display:'flex', justifyContent:'space-around',  alignContent:'center'}}>
+                                            <Box sx={{display:'flex', justifyContent:'center', }}>
+                                                <Typography variant="h5" display='flex' flex={8} >Materia</Typography>
+                                            </Box>
+                                            <Box sx={{display:'flex', justifyContent:'center',}}>
+                                            <Typography variant="h5" display='flex' flex={8} >{course.subjects[0]}</Typography>
+                                            </Box>
+                                        </Box>
+                                        <Box p={2} sx={{display:'flex', justifyContent:'space-around',  alignContent:'center'}}>
+                                            <Box sx={{display:'flex', justifyContent:'center', }}>
+                                                <Typography variant="h5" display='flex' flex={8} >Duracion</Typography>
+                                            </Box>
+                                            <Box sx={{display:'flex', justifyContent:'center',}}>
+                                            <Typography variant="h5" display='flex' flex={8} >{course.frecuency}</Typography>
+                                            </Box>
+                                        </Box>
+                                        <Box p={2} sx={{display:'flex', justifyContent:'space-around',  alignContent:'center'}}>
+                                            <Box sx={{display:'flex', justifyContent:'center', }}>
+                                                <Typography variant="h5" display='flex' flex={8} >Tipo de Clase</Typography>
+                                            </Box>
+                                            <Box sx={{display:'flex', justifyContent:'center',}}>
+                                            <Typography variant="h5" display='flex' flex={8} >{course.classType}</Typography>
+                                            </Box>
+                                        </Box>
+                                        <Box p={2} sx={{display:'flex', justifyContent:'space-around',  alignContent:'center'}}>
+                                            <Box sx={{display:'flex', justifyContent:'center', }}>
+                                                <Typography variant="h5" display='flex' flex={8} >Costo</Typography>
+                                            </Box>
+                                            <Box sx={{display:'flex', justifyContent:'center',}}>
+                                            <Typography variant="h5" display='flex' flex={8} >{'$' + course.price}</Typography>
+                                            </Box>
+                                        </Box>
+                                    </Stack>
                                 </Container>
                             </Box>
                         </Box>
+                            <Container ><Divider/></Container>
                         <Box>
                             <Container>
-                                Descripcion
+                                <Box py={2}>
+                                    <Typography variant="h5" display='flex' flex={8} >
+                                        Descripción del Curso
+                                    </Typography>
+                                    <TextField
+                                        id="outlined-multiline-static"
+                                        label="Multiline"
+                                        fullWidth 
+                                        disabled 
+                                        multiline
+                                        rows={4}
+                                        defaultValue="Default Value"
+                                    />
+                                </Box>
                             </Container>
                         </Box>
                     </Paper>
