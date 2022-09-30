71bd30b682a7c13ab1476ca487488abaf4588434
ZetuK <matefed@gmail.com>
2022-09-30 17:59:22 -0300

    Agregado imagen, traduccion a español y cambios de nombre

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -25,7 +25,15 @@ return (<Container component="main" maxWidth="xs">
     Bienvenido
 </Typography>
 <Typography sx={{color:'black',fontSize:24}}>
-    Bienvenido a la web de Sitio Institucional! En esta web podra registrarse como alumno o profesor. Una vez registrados, los profesores podran crear clases de distintos tipos de modalidades y costos y los alumnos podran inscribirse a ellas. La plataforma tambien tiene un sistema de califcacion de clases y comentarios.
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+luctus ut est sed faucibus. Duis bibendum ac ex vehicula laoreet.
+Suspendisse congue vulputate lobortis. Pellentesque at interdum
+tortor. Quisque arcu quam, malesuada vel mauris et, posuere
+sagittis ipsum. Aliquam ultricies a ligula nec faucibus. In elit
+metus, efficitur lobortis nisi quis, molestie porttitor metus.
+Pellentesque et neque risus. Aliquam vulputate, mauris vitae
+tincidunt interdum, mauris mi vehicula urna, nec feugiat quam
+lectus vitae ex.
 </Typography>
 <Button variant='outlined' onClick={()=>history("/login")} sx={{marginTop:"1rem",maringBottom:"1rem",padding:"1rem",borderRadius:"12px",backgroundColor:"white",width:"200px"}}>
 Continuar
