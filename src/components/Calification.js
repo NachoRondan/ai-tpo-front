dcef28ca558b77be18d07ce573213cb81024b261
Nacho Rondan <nacho.rondan@gmail.com>
2022-10-18 12:25:53 -0300

    Elimino value innecesario

--- src/components/Calification.js
+++ src/components/Calification.js
@@ -4,7 +4,7 @@ import StarRateIcon from '@mui/icons-material/StarRate';
 export default function Calification(props) {
     const roundedCalification = Math.round(props.calification)
     return (
-        <Box width={1}  sx={{ flexDirection: 'row' }} value={roundedCalification}>
+        <Box width={1}  sx={{ flexDirection: 'row' }}>
                 {Array.from(Array(roundedCalification)).map(( _, index) => (
                     <StarRateIcon style={{ color: '#fabb05' }}/>
                 ))}
