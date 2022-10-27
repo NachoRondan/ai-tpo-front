da557bd27c31d6c79287701348064727a663bfa7
Nacho Rondan <nacho.rondan@gmail.com>
2022-10-27 10:04:34 -0300

    Agrego pagina de contrataciones

---
+++ src/assets/MockUpVariables/MockUpContrataciones.js
@@ -0,0 +1,22 @@
+import User3Picture from '../MockUpFiles/user3.jpg'
+
+export default function getContrataciones(){
+    const contrataciones = [
+        {
+            userName: 'Ignacio Rondan',
+            picture:User3Picture, 
+            comment: 'Quiero tener esta clase, me interesa',
+            phoneNumber: '1161807735',
+            email:'mailFalse@gmail.com',
+        },
+        {
+            userName: 'Ignacio Rondan',
+            picture:User3Picture, 
+            comment: 'Quiero tener esta clase, me interesa',
+            phoneNumber: '1161807735',
+            email:'mailFalse@gmail.com',
+        }
+    ]
+
+    return contrataciones
+}
---
+++ src/components/ContratacionesFeed.js
@@ -0,0 +1,27 @@
+import { Stack, Box, Typography, Divider, Button, TextField } from "@mui/material";
+import getContrataciones from "../assets/MockUpVariables/MockUpContrataciones";
+
+
+export default function ContratacionesFeed({user}){
+    const contrataciones = getContrataciones()
+    
+    return(
+        <Stack divider={<Divider  />} spacing={2} >
+            {contrataciones.map((contratacion) => (
+                <Box>
+                    <Stack alignContent='center' justifyContent='flex-start'> 
+                        <Typography marginBottom={2} variant="h5">{contratacion.userName}</Typography>
+                        <TextField 
+                            value={contratacion.comment}
+                            multiline
+                            disabled
+                        />
+                    <Box marginTop={2} felx={12} sx={{display:'flex', justifyContent:'center'}}>
+                        <Button variant="contained" color="secondary">Datos de Contacto</Button>
+                    </Box>
+                    </Stack>
+                </Box>
+            ))}
+        </Stack>
+    )
+}
